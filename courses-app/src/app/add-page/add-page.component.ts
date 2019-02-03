05dbcd8ae087890181ffa56f7a68cfa7789c7bfb
Oleg Koleda <olegator2510@mail.ru>
2019-02-03 16:54:30 +0300

    courses-app: feature - implemented routing, added new pages

--- courses-app/src/app/add-page/add-page.component.ts
+++ courses-app/src/app/add-page/add-page.component.ts
@@ -1,23 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CoursesService } from '../services/courses.service';
+import { Course } from '../courses-list/course.module';
 
 @Component({
   selector: 'app-add-page',
   templateUrl: './add-page.component.html',
   styleUrls: ['./add-page.component.scss']
 })
-export class AddPageComponent {
+export class AddPageComponent implements OnInit {
   public title: string;
   public description: string;
   public date: string;
   public duration: number;
+  public courseId: number;
+  public courseData: Course;
 
-  constructor() { }
+
+  constructor(private route: ActivatedRoute, private coursesService: CoursesService,
+              private router: Router) { }
+
+  ngOnInit() {
+    this.route.params.subscribe((data) => {
+      this.courseId = data['id'];
+      if (this.courseId) {
+        this.courseData = this.coursesService.getById(this.courseId);
+        console.log(this.courseData);
+        // // tslint:disable-next-line:no-unused-expression
+        // { this.title, this.description, this.date, this.duration} = this.courseData;
+        // this.title = this.courseData.title;
+      }
+    });
+
+
+  }
 
   public save() {
-    console.log("Saved");    
+    console.log('Saved');
+    this.router.navigate(['courses']);
   }
 
   public cancel() {
-    console.log("Canceled");
+    console.log('Canceled');
+    this.router.navigate(['courses']);
   }
 }
--- courses-app/src/app/app-routing.module.ts
+++ courses-app/src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { CoursesListComponent } from './courses-list/courses-list.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { AddPageComponent } from './add-page/add-page.component';
+import { NotExistingPageComponent } from './not-existing-page/not-existing-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'courses', pathMatch: 'full' },
-  { path: 'courses', component: CoursesListComponent },
   { path: 'login', component: LoginPageComponent },
-  { path: 'add', component: AddPageComponent },
+  { path: 'courses', component: CoursesListComponent },
+  { path: 'courses/new', component: AddPageComponent },
+  { path: 'courses/:id', component: AddPageComponent },
+      /// 404
+    { path: '**', component: NotExistingPageComponent }
 ];
 
 @NgModule({
